112a9fd8af8dfa2e2cb451216f5ad43d867ddc9c
Chris-Maskey <chrismaskey03@gmail.com>
2024-08-24 14:48:12 +0000

    Initial Commit

---
+++ lib/initial-profile.ts
@@ -0,0 +1,34 @@
+import { currentUser, redirectToSignIn } from "@clerk/nextjs/server";
+
+import { db } from "@/lib/db";
+import { Profile } from "@prisma/client";
+
+export const initialProfile = async (): Promise<Profile> => {
+  const user = await currentUser();
+
+  if (!user) {
+    redirectToSignIn();
+    throw new Error("User not authenticated.");
+  }
+
+  const profile = await db.profile.findUnique({
+    where: {
+      userId: user.id,
+    },
+  });
+
+  if (!profile) {
+    const newProfile = await db.profile.create({
+      data: {
+        userId: user.id,
+        name: `${user.firstName} ${user.lastName}`,
+        imageUrl: user.imageUrl,
+        email: user.emailAddresses[0].emailAddress,
+      },
+    });
+
+    return newProfile;
+  }
+
+  return profile;
+};
